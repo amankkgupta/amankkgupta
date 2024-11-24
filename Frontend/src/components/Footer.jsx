7f67d99ac15018acde26befb1552ded567338de0
Aman Kumar Gupta <amangupta65734@gmail.com>
2024-11-24 14:03:00 +0530

    completed

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-600 text-white py-4">
+      <div className="container flex flex-col gap-4 text-center">
+        <p>Email: <a href="mailto:amangupta65734@gmail.com" className="text-white underline">amangupta65734@gmail.com</a></p>
+        <p>&copy; {new Date().getFullYear()} Aman Kumar Gupta. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
